6ae30dae2c2883a0659787e7f71e98fff0fd9988
Justin <justin1998m1@gmail.com>
2021-12-19 03:42:10 -0500

    better visuals and html injection from json

---
+++ js/addphish.js
@@ -0,0 +1,14 @@
+function addphish (){
+console.log("Hello There, I'm Running")
+console.log("Up Next \n -mobile friend visuals \n creating script to be ran to take input and chang json file");
+console.log("Add Delete button to each song");
+console.log("Then send to sever to update json");
+console.log('Getting https pleaseee');
+console.log("Uploading files to s3");
+console.log("Script to upload to s3 when json update happens");
+console.log("can ya buy phishPizza?");
+}
+
+var form = document.getElementById('form')
+form.addEventListener('submit',addphish)
+console.log('hello');
